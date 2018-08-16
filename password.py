fe3f1aa1f97ee1ab65f3afe15b9550e2f61af583
jc432986 <jack.camp@my.jcu.edu.au>
2018-08-16 13:31:02 +1000

    Put in password check

--- password.py
+++ password.py
@@ -1,1 +1,8 @@
 """Jack Camp"""
+MINIMUM_PASSWORD_LENGTH = 4
+password = str(input("Please enter a password "))
+password_length = len(password)
+while password_length <= MINIMUM_PASSWORD_LENGTH:
+    password = str(input("please enter a password "))
+    password_length = len(password)
+print("*"*password_length)
